8a3cd350233ef397f0d710fe80e32d67afb9a8cd
sp2363 <maksim.honchar@gmail.com>
2025-08-10 15:37:40 +0300

    added button to top

--- js/main.js
+++ js/main.js
@@ -2,3 +2,28 @@ const btn = document.querySelector(".btn-menu");
 const nav = document.querySelector(".header__nav");
 
 btn.addEventListener("click", () => nav.classList.toggle("open-menu"));
+
+
+
+// Отримуємо кнопку
+const backToTopBtn = document.querySelector('.back-to-top-btn');
+
+// Визначаємо висоту вікна перегляду
+const viewportHeight = window.innerHeight;
+
+// Встановлюємо поріг для появи кнопки, наприклад, 50% від висоти вікна
+const threshold = viewportHeight * 0.5; // 50% від висоти вікна
+
+// Додаємо обробник подій на скрол
+window.addEventListener('scroll', () => {
+    // Перевіряємо, чи прокручена сторінка за поріг
+    if (window.scrollY > threshold) {
+        // Якщо так, робимо кнопку видимою
+        backToTopBtn.classList.remove('hidden');
+    } else {
+        // Якщо ні, приховуємо її
+        backToTopBtn.classList.add('hidden');
+    }
+});
+
+
